d3985629e94f1a68e0007b09286349041ea953e8
rinesagajtani <rinesagajtani0@gmail.com>
2025-12-09 01:54:08 +0100

    set up sass compilation via webpack.

---
+++ webpack.config.cjs
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  experiments: {
+    outputModule: true
+  }
+};
