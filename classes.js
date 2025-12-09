b0de648a407f2c6bf7610427a0dcc77dd262b0e1
rinesagajtani <rinesagajtani0@gmail.com>
2025-12-09 01:10:26 +0100

    add Marker class with color field

--- classes.js
+++ classes.js
@@ -1,3 +1,13 @@
+// 1) Implement a class that describes a circle. It must have the following components:
+
+// a field that stores the radius of the circle;
+// get-property that returns the radius of the circle;
+// set-property that sets the radius of the circle;
+// get-property that returns the diameter of the circle;
+// a method that calculates the area of a circle;
+// a method that calculates the length of the circle.
+// Demonstrate the work of properties and methods.
+
 class Circle {
   constructor(radius) {
     this._radius = radius;
@@ -34,3 +44,18 @@ document.write('<div class="line">Diameter: ' + circle.diameter + '</div>');
 document.write('<div class="line">Area: ' + circle.getArea().toFixed(2) + '</div>');
 document.write('<div class="line">Circumference: ' + circle.getCircumference().toFixed(2) + '</div>');
 document.write('</div>');
+
+// 2) Implement a class that describes a stationery marker. It must have the following components:
+
+// a field that stores the color of the marker;
+// a field that stores the amount of ink in the marker (in percent);
+// an input method (accepts a string and prints the text in the corresponding color; the text is displayed as long as there is ink in the marker; one non-whitespace character is 0.5% of the ink in the marker).
+// Implement a class that describes a refillable marker. Inherit this class from a simple marker and add a method for refilling.
+
+// Demonstrate the work of the written methods.
+
+ class Marker {
+  constructor(color) {
+    this.color = color;
+  }
+}
